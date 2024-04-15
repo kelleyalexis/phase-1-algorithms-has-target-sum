17d6355002e498152dd93872e3ad0a3b25da9b30
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-04-15 15:33:29 -0400

    Learning the BigO

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+ 
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+      return true;
+      }
+    }
+  }
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
+/*
+  O(n^2)
+  O(1)
+  ---revisit this for better understanding
 */
 
 /* 
-  Add your pseudocode here
+  funtion takes in two arguments: an `array` and the `target index`
+  
+  if (i = 0, i < x, i++) and (j = i + 1, j < x, j++)
+  
+  
+  if the sum of two indices === target sum
+  if i + j === target
+  return true
+  else return false
 */
 
 /*
-  Add written explanation of your solution here
+  the function takes in two arguments, an array and a target 
+  using the if loop to iterate the first half of the array then the second half of the array
+  to evaluate if the sum of any two indices is equal to the target.
+  if they are equal returns true otherwise returns false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 1300, 3800, 11, 1200], 5000));
+
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([123456, 9876, 739, 6598, 64523,], 198273));
+
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
